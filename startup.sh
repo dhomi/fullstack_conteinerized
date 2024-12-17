96e4e39e23919a5dc175135434a63be9b4fac6ed
Steven Nienhuis <steven@qualityaccelerators.nl>
2024-12-17 11:13:35 +0100

    startup.sh uitgebreid met een auto install van helm alleen als het nodig is, maar ook nog niet getest.

--- startup.sh
+++ startup.sh
@@ -1,25 +1,32 @@
 #!/usr/bin/env bash
 
-# install helm first
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
+# Check if Helm is installed
+if ! helm version > /dev/null 2>&1; then
+    echo "Helm is not installed. Installing Helm..."
 
-# create both namespaces
+    # Install Helm
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+    chmod 700 get_helm.sh
+    ./get_helm.sh
+else
+    echo "Helm is already installed."
+fi
+
+# Create both namespaces
 kubectl create namespace techlab
 kubectl apply -f deployment.yaml -n techlab
 kubectl -n techlab get pods
 kubectl create ns chaos-mesh
 
+# Add Helm repo and install/upgrade Chaos Mesh
 helm repo add chaos-mesh https://charts.chaos-mesh.org
 helm install chaos-mesh chaos-mesh/chaos-mesh -n=chaos-mesh --version 2.7.0 &
 helm upgrade chaos-mesh chaos-mesh/chaos-mesh -n=chaos-mesh --version 2.7.0 --set dashboard.securityMode=false
 
-# todo: hier moet de terminal effe een tijd wachten totdat alles hierboven is uitgevoerd 
-# anders start de port forwarding maar de services is nog down...
-sleep 2m # Waits 2 minutes.
+# Wait for 2 minutes to ensure services are up
+sleep 2m
 
-# port forwarding
+# Port forwarding
 kubectl port-forward -n chaos-mesh svc/chaos-dashboard 2333:2333 &
 kubectl port-forward -n techlab svc/middleware-fastapi 8000:8000 &
 kubectl port-forward -n techlab svc/grafana 4000:4000 &
