dfd138bd532c3818ce0b5cf84b1a1e276fc916b4
Beni <gitkraken@dhomi.nl>
2025-01-14 09:59:58 +0100

    add db port-forwarding too

--- startup.cmd
+++ startup.cmd
@@ -33,6 +33,7 @@ start /b kubectl port-forward -n chaos-mesh svc/chaos-dashboard 2333:2333
 start /b kubectl port-forward -n techlab svc/middleware-fastapi 8000:8000
 start /b kubectl port-forward -n techlab svc/grafana 4000:4000
 start /b kubectl port-forward -n techlab svc/frontend-django 8001:8001
+start /b kubectl port-forward -n techlab svc/db 3306:3306
 
 REM Houd de terminal open
 pause
